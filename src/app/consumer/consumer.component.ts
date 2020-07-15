d5480b3bd49accb0925a162f266b92510ba342ea
Fernando Hernandez <hernandezfernando18982012@gmail.com>
2020-07-14 23:02:42 -0600

    visualizacion de datos de registro y refresh de datasource

--- src/app/consumer/consumer.component.ts
+++ src/app/consumer/consumer.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ChangeDetectorRef } from '@angular/core';
 import { ApiService } from '../api.service';
 import { Cases } from '../cases';
 import { MatPaginator } from '@angular/material/paginator';
-
+import { MatTableDataSource } from '@angular/material/table';
 @Component({
   selector: 'app-consumer',
   templateUrl: './consumer.component.html',
@@ -24,14 +24,28 @@ export class ConsumerComponent implements OnInit {
     status: null,
     updated: null,
   };
+  viewelement: Cases = {
+    _id: null,
+    name: null,
+    gender: null,
+    age: null,
+    address: null,
+    city: null,
+    country: null,
+    status: null,
+    updated: null,
+  };
+  selectelement = false;
   displayedColumns: string[] = ['_id', 'name', 'gender', 'country', 'Acciones'];
-  constructor(private apiService: ApiService) { }
+  dataSource = new MatTableDataSource(this.elementos);
+  constructor(private apiService: ApiService, private changeDetectorRefs: ChangeDetectorRef) { }
   ngOnInit() {
     this.obtenerdatos();
   }
   obtenerdatos() {
     this.apiService.getall().subscribe((elementos: Cases[]) => {
       this.elementos = elementos;
+      this.dataSource.data = elementos;
       console.log(this.elementos);
     });
   }
@@ -48,7 +62,7 @@ export class ConsumerComponent implements OnInit {
       updated: null,
     };
   }
-  createOrUpdate(form: any ) {
+  createOrUpdate(form: any) {
     if (this.selectedelement && this.selectedelement._id) {
       form.value.id = this.selectedelement._id;
       this.apiService.update(this.selectedelement._id, form.value).subscribe((elemento: Cases) => {
@@ -62,13 +76,22 @@ export class ConsumerComponent implements OnInit {
     this.clear();
     this.obtenerdatos();
     this.selectedIndex = 0;
+    this.close();
+
   }
 
   select(elemento: Cases) {
     this.selectedelement = elemento;
     this.selectedIndex = 1;
   }
-
+  view(elemento: Cases) {
+    this.selectelement = true;
+    this.viewelement = elemento;
+  }
+  close() {
+    this.selectelement = false;
+    this.viewelement = null;
+  }
   delete(id: number) {
     this.apiService.delete(id).subscribe((elemento: Cases) => {
       console.log('elemento eliminado', elemento);
